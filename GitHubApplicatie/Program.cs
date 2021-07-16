76ac671f0142fd3f70022c70aa1d20803a917754
katia verachtert <katia.verachtert.scherpendries18@outlook.com>
2021-07-16 11:30:06 +0200

    Add project files.

---
+++ GitHubApplicatie/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitHubApplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Geef leeftijd");
+            int leeftijd = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
