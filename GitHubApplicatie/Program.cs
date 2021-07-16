5995d9583236d6d23549af77ae33d5db472a2e32
katia verachtert <katia.verachtert.scherpendries18@outlook.com>
2021-07-16 11:34:35 +0200

    naam toevoegen afgewerkt

--- GitHubApplicatie/Program.cs
+++ GitHubApplicatie/Program.cs
@@ -6,7 +6,8 @@ namespace GitHubApplicatie
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Naam: ");
+            string naam = Console.ReadLine();
             Console.WriteLine("Geef leeftijd");
             int leeftijd = Convert.ToInt32(Console.ReadLine());
         }
